c2cf3586c89e04bc9dc9523dd78ecc9d84803079
okaraduman <oguzhan.kduman@gmail.com>
2022-06-28 23:20:18 +0100

    initialize

---
+++ src/main/java/com/product/salescheck/ProductSalesCheckApplication.java
@@ -0,0 +1,13 @@
+package com.product.salescheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSalesCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSalesCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/product/salescheck/ProductSalesCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.salescheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSalesCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
